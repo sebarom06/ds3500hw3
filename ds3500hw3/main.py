4d2547e852467dc1a87b5f4ff90f3f7e8a2c0318
Caitlin Heery <caitlin05aki@gmail.com>
2026-02-20 16:43:47 -0500

    added function and layout of main

--- ds3500hw3/main.py
+++ ds3500hw3/main.py
@@ -2,3 +2,22 @@ import panel as pn
 import Scatter_Plot as sc
 import Cleaning as c
 
+api = c.DfCleaner(c.file_path)
+
+def get_scatter_dataset():
+    pass
+
+def get_scatter():
+    pass
+
+def get_bar_dataset():
+    pass
+
+def get_bar():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
